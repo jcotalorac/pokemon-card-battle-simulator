b723a3022580628b8775a20972c2817426afaaed
Juan Carlos Otálora Caro <jcotalorac@gmail.com>
2024-08-09 20:40:46 -0500

    Exposing endpoint to get battles

---
+++ src/main/java/org/pokemon/controllers/BattleController.java
@@ -0,0 +1,15 @@
+package org.pokemon.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class BattleController {
+
+    @GetMapping("/battles")
+    public void getBattles(){
+        log.debug("Getting battles");
+    }
+}
