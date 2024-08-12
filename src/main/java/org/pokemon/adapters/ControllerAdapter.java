1315d55dfc3198334cad6c90129e51a3aeb3002f
Juan Carlos Otálora Caro <jcotalorac@gmail.com>
2024-08-12 10:37:43 -0500

    Additional mappings to accomplish complete conversion data from service to presentation

--- src/main/java/org/pokemon/adapters/ControllerAdapter.java
+++ src/main/java/org/pokemon/adapters/ControllerAdapter.java
@@ -3,14 +3,26 @@ package org.pokemon.adapters;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.pokemon.controllers.dto.BattlePlayer;
+import org.pokemon.controllers.dto.BattleResponse;
 import org.pokemon.controllers.dto.PokemonCardResponse;
+import org.pokemon.domain.Battle;
+import org.pokemon.domain.Player;
 import org.pokemon.domain.PokemonCard;
+
+import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ControllerAdapter {
 
     @Mapping(target = "attackPoints", source = "attack")
-    PokemonCardResponse map(PokemonCard pokemonCard);
+    PokemonCardResponse mapCard(PokemonCard pokemonCard);
 
+    List<PokemonCardResponse> mapCards(List<PokemonCard> pokemonCards);
+
+    @Mapping(target = "pokemonCardResponses", source = "pokemonCards")
+    BattlePlayer mapPlayer(Player player);
+
+    BattleResponse mapBattle(Battle battle);
 
 }
