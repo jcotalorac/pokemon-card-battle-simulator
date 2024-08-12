c91b9e487f56c2b14301cd8fbfb7609391adf55d
Juan Carlos Otálora Caro <jcotalorac@gmail.com>
2024-08-12 17:51:08 -0500

    Fixing size of playing cards

--- src/main/java/org/pokemon/services/impl/BattleServiceImpl.java
+++ src/main/java/org/pokemon/services/impl/BattleServiceImpl.java
@@ -3,12 +3,18 @@ package org.pokemon.services.impl;
 import org.pokemon.domain.Battle;
 import org.pokemon.domain.PokemonCard;
 import org.pokemon.services.BattleService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Service
 public class BattleServiceImpl implements BattleService {
+
+    @Value("${cardsSize}")
+    private Integer cardsSize;
 
     @Override
     public Battle play() {
@@ -22,8 +28,16 @@ public class BattleServiceImpl implements BattleService {
         card2.setHealthPoints(68);
         card2.setAttack(81);
 
-        battle.getComputerPlayer().setPokemonCards(List.of(card1));
-        battle.getHumanPlayer().setPokemonCards(List.of(card2));
+        List<PokemonCard> humanCards = new ArrayList<>(cardsSize);
+        List<PokemonCard> computerCards = new ArrayList<>(cardsSize);
+
+        IntStream.range(0, cardsSize).forEach(i -> {
+            humanCards.add(card1);
+            computerCards.add(card2);
+        });
+
+        battle.getHumanPlayer().setPokemonCards(humanCards);
+        battle.getComputerPlayer().setPokemonCards(computerCards);
 
         return battle;
     }
--- src/test/java/org/pokemon/services/impl/BattleServiceImplTest.java
+++ src/test/java/org/pokemon/services/impl/BattleServiceImplTest.java
@@ -4,16 +4,21 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.pokemon.domain.Battle;
 import org.pokemon.services.BattleService;
+import org.springframework.test.util.ReflectionTestUtils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BattleServiceImplTest {
 
+    private final Integer cardsSize = 4;
     private BattleService battleService;
 
     @BeforeEach
     public void setUp() {
+
         battleService = new BattleServiceImpl();
+        ReflectionTestUtils.setField(battleService, "cardsSize", cardsSize);
     }
 
     @Test
@@ -26,5 +31,7 @@ public class BattleServiceImplTest {
         assertNotNull(battle.getComputerPlayer(), "Computer player is not initialized");
         assertNotNull(battle.getHumanPlayer().getPokemonCards(), "Human player must have dealt cards");
         assertNotNull(battle.getHumanPlayer().getPokemonCards(), "Computer player must have dealt cards");
+        assertEquals(cardsSize, battle.getHumanPlayer().getPokemonCards().size());
+        assertEquals(cardsSize, battle.getComputerPlayer().getPokemonCards().size());
     }
 }
